c3c5fbd9c15f03f5282aa4d197b5864047c2ac27
Sigui Kessé Emmanuel <contact@sigui.ci>
2024-03-23 20:54:50 +0100

    :heavy_plus_sign: Use boss.sh

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,19 @@
-import { plugin, which } from "bun";
+import { register as boss_register, load } from "boss.sh";
 
-export async function use(
-  path: string,
-  data: object = {},
-  env = Bun.env,
-): Promise<object> {
-  const file = Bun.file(path);
-  if (!file.exists()) {
-    throw new Error(`No such file ${path}`);
-  }
+export async function use(path: string, data?: object): Promise<any> {
+  const json = JSON.stringify(data ?? {});
 
-  const php = which("php");
-  if (!php) {
-    throw new Error("Cannot find PHP in your PATH");
-  }
-
-  const json = JSON.stringify(data);
-  const { success, stdout, stderr } = Bun.spawnSync([
+  return await load(path, [
     "php",
     "-r",
     `$data = json_decode('${json}', true); if(is_array($data) && !\\array_is_list($data)) extract($data); ob_start(); $default = require "${path}"; if($default === 1) $default = ob_get_clean(); else ob_end_clean(); echo json_encode($default);`,
   ]);
-  if (!success) {
-    throw new Error(`Failed to run ${path}: ${stderr.toString()}`);
-  }
-
-  const result = stdout.toString();
-  return JSON.parse(result);
 }
 
-export function register(name = "PHP Loader", filter: RegExp = /\.php$/): void {
-  plugin({
-    name,
-    async setup(build) {
-      build.onLoad({ filter }, async ({ path }) => {
-        const result = await use(path);
-        const exports =
-          typeof result === "object" ? result : { default: result };
-        return {
-          exports,
-          loader: "object",
-        };
-      });
-    },
-  });
+export function register(name = "PHP Loader", filter: RegExp = /\.php$/) {
+  boss_register(name, filter, (path) => [
+    "php",
+    "-r",
+    `$data = ob_start(); $default = require "${path}"; if($default === 1) $default = ob_get_clean(); else ob_end_clean(); echo json_encode($default);`,
+  ]);
 }
